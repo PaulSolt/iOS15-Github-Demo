6aea8090fe614544995066a2de4a94a0d39a6c42
Paul Solt <PaulSolt@gmail.com>
2020-05-15 15:26:29 -0400

    Added starter project

---
+++ GithubDemoApp/GithubDemoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GithubDemoApp
+//
+//  Created by Paul Solt on 5/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ GithubDemoApp/GithubDemoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubDemoApp
+//
+//  Created by Paul Solt on 5/15/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
